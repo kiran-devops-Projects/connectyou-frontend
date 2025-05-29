68661efe585f82b0fb32905213d0552c53583984
sudarshanJ18 <jsudarshanreddy2003@gmail.com>
2025-05-30 00:12:57 +0530

    Only readme

--- src/components/lib/utils.js
+++
@@ -1,8 +0,0 @@
-export const formatDate = (date) => {
-    return new Date(date).toLocaleDateString();
-  };
-  
-  export const capitalize = (string) => {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  };
-  
